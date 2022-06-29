1dfb72b1b80dc76b6f413c965d8ff721e4f3af47
Renaldas Bendikas <renaldasb1999@gmail.com>
2022-06-29 16:25:58 +0000

    add function for tracking sunken ships

--- game.py
+++ game.py
@@ -33,17 +33,28 @@ class Battleships():
         return self.board
 
     def get_player_input(self):
+        """ Function to validate user input, row must be a number
+         between 0 and 5, column must be a letter between A and F according to 
+         English Alphabetical order"""
         try:
             x_row = input("Guess the row of enemy ship: ")
             while x_row not in '12345':
                 print('Please select a value between 0 and 5')
                 x_row = input("Guess the row of enemy ship: ")
 
-            y_column = input('Guess the column letter of enemy ship: ')
+            y_column = input('Guess the column letter of enemy ship: ').upper()
             while y_column not in 'ABCDEF':
                 print('Please select a value between A,B,C,D,E or F')
-                y_column = input('Guess the column letter of enemy ship: ')
+                y_column = input('Guess the column letter of enemy ship: ').upper()
             return int(x_row) - 1, GameBoard.convert_letters_to_nums()[y_column]
         except ValueError and KeyError:
             print('Not a valid input')
             return self.get_player_input()
+
+    def count_sunk_ships(self):
+        sunk_ships = 0
+        for row in self.board:
+            for column in row:
+                if column == "X":
+                    sunk_ships += 1
+        return sunk_ships
