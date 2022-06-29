8be213465642801456866f9ded2402c28a01b04b
Renaldas Bendikas <renaldasb1999@gmail.com>
2022-06-29 15:22:43 +0000

    add print board function for board layout

--- game.py
+++ game.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+class GameBoard:
+    """
+    The GameBoard class is used to create the playing board
+    """
+
+    def __init__(self, board):
+        self.board = board
+
+    def convert_letters_to_nums():
+        letters_to_nums = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4,"F": 5,}
+        return letters_to_nums
+
+    def print_board(self):
+        print("  A B C D E F")
+        print("  +-+-+-+-+-+")
+        row_number = 1
+        for row in self.board:
+            print("%d|%s|" % (row_number, "|".join(row)))
+            row_number += 1
