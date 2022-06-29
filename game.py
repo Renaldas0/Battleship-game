7c5a805bc97f32e8384c679b3341273e27718905
Renaldas Bendikas <renaldasb1999@gmail.com>
2022-06-29 15:38:48 +0000

    add Battleships class and create the ships on the board marked with X

--- game.py
+++ game.py
@@ -19,3 +19,15 @@ class GameBoard:
         for row in self.board:
             print("%d|%s|" % (row_number, "|".join(row)))
             row_number += 1
+
+class Battleships():
+    def __init__(self, board):
+        self.board = board
+
+    def create_ships(self):
+        for i in range(4):
+            self.x_row, self.y_column = random.randint(0, 5), random.randint(0, 7)
+            while self.board[self.x_row][self.y_column] == 'X':  #while loop to check if there is something placed there
+                self.x_row, self.y_column = random.randint(0, 7), random.randint(0, 7)
+            self.board[self.x_row][self.y_column] = "X"
+        return self.board
